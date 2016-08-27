713838cb26e75d47b364b834b9898d10ba7ecba7
harshit <harshitaggarwal283@yahoo.com>
2016-08-27 10:14:27 +0530

    mobile responsive form

---
+++ main.js
@@ -0,0 +1,139 @@
+
+$('#clg1').click(function () {
+    $('#clgname').val("INDIAN INSTITUTE OF TECHNOLOGY,DELHI")
+});
+
+$('#clg2').click(function () {
+    $('#clgname').val("DELHI TECHNOLOGICAL UNIVERSITY")
+});
+$('#clg3').click(function () {
+    $('#clgname').val("NETAJI SUBHASH INSTITUE OF TECHNOLOGY")
+});
+
+$('#clg4').click(function () {
+    $('#clgname').val("INDRAPRASHTA INSTITUE OF INFORMATION TECHNOLOGY")
+});
+
+$('#clg5').click(function () {
+    $('#clgname').val("BILRA INSTITUTE OF TECHNOLOGY")
+});
+
+$('#clg6').click(function () {
+    $('#clgname').val("")
+});
+
+
+
+$('#b1').click(function () {
+    $('#branch').val('COMPUTER SCIENCE AND ENGINEERING');
+});
+
+$('#b2').click(function () {
+    $('#branch').val("INFORMATION TECHNOLOY");
+});
+
+$('#b3').click(function () {
+    $('#branch').val("SOFTWARE ENGINEERING");
+});
+
+$('#b4').click(function () {
+    $('#branch').val("ELECTRONICS AND COMMUNICATIONS ENGINEERING");
+});
+
+$('#b5').click(function () {
+    $('#branch').val("ELECTRICAL AND ELECTRONICS ENGINEERING");
+});
+
+$('#b6').click(function () {
+    $('#branch').val("MECHANICAL ENGINEERING");
+});
+
+
+$('#y1').click(function () {
+    $('#year').val("1st YEAR")
+});
+
+$('#y2').click(function () {
+    $('#year').val("2nd YEAR")
+});
+
+$('#y3').click(function () {
+    $('#year').val("3rd YEAR")
+});
+
+$('#y4').click(function () {
+    $('#year').val("4th YEAR")
+});
+
+
+$('#1').click(function () {
+    $('#course').val("Launchpad");
+});
+
+$('#2').click(function () {
+    $('#course').val("Crux")
+});
+
+$('#3').click(function () {
+    $('#course').val("Node Js")
+});
+
+$('#4').click(function () {
+    $('#course').val("Angular Js")
+});
+
+$('#5').click(function () {
+    $('#course').val("Django")
+});
+
+$('#6').click(function () {
+    $('#course').val("Android")
+});
+
+$('#7').click(function () {
+    $('#course').val("IOS")
+});
+
+$('#8').click(function () {
+    $('#course').val("Machine Learning")
+});
+
+$('#9').click(function () {
+    $('#course').val("Game Development")
+});
+
+$('#10').click(function () {
+    $('#course').val("Launchpad");
+});
+
+$('#11').click(function () {
+    $('#course').val("Crux")
+});
+
+$('#12').click(function () {
+    $('#course').val("Node Js")
+});
+
+$('#13').click(function () {
+    $('#course').val("Angular Js")
+});
+
+$('#14').click(function () {
+    $('#course').val("Django")
+});
+
+$('#15').click(function () {
+    $('#course').val("Android")
+});
+
+$('#16').click(function () {
+    $('#course').val("IOS")
+});
+
+$('#17').click(function () {
+    $('#course').val("Machine Learning")
+});
+
+$('#18').click(function () {
+    $('#course').val("Game Development")
+});
